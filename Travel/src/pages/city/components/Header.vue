b00b6554d91319599c664a5cf59e16b58ad9afb5
phelps <phelps@phelpsdeMacBook-Pro.local>
2019-05-05 22:36:56 +0800

    city router

---
+++ Travel/src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+        <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #ffffff
+    background $bgColor
+    font-size .36rem
+    .header-back
+        left 0
+        top 0
+        position absolute
+        width 0.64rem
+        text-align center
+        font-size  .4rem
+        color #fff
+</style>
--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -6,9 +6,11 @@
       <div class="header-input">
           <span class="iconfont">&#xe632;</span>
           输入城市/景点/游玩</div>
+          <router-link to="/city" >
       <div class="header-right">{{this.city}}
           <span class="iconfont arrow-icon">&#xe638;</span>
       </div>
+      </router-link>
   </div>
 </template>
 
@@ -25,7 +27,7 @@ export default {
 @import '~styles/varibles.styl'
 .header
     display flex
-    line-height .86rem
+    line-height $headerHeight
     background $bgColor
     color #fff
     .header-left
@@ -49,6 +51,7 @@ export default {
         padding 0 .1rem
         float right 
         text-align center
+        color #fff 
         .arrow-icon
             margin-left -.04rem
             font-size .24rem
